9c19407dda7d17a604d1b7066b221addc95c25e9
Mary White <mary.a.white28@gmail.com>
2022-01-07 13:20:01 -0600

    created basic registration form

---
+++ src/main/java/com/quasar/registrationform/MainController.java
@@ -0,0 +1,29 @@
+package com.quasar.registrationform;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/register")
+    public String showForm(Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+        List<String> professionList = Arrays.asList("Developer", "Designer", "Engineer", "Instructor", "Manager", "Chief Information Officer");
+        model.addAttribute("professionList", professionList);
+        return "register_form";
+    }
+
+    @PostMapping("/register/success")
+    public String submitForm(@ModelAttribute("user") User user){
+        System.out.println(user);
+        return "register_success";
+    }
+}
